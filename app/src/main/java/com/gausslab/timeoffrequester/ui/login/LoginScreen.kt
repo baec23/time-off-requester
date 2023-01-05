4c3351be70eadff1487ec97015f37cdeb2dde959
dawn <wi825@naver.com>
2023-01-05 18:29:00 +0900

    Login view

--- app/src/main/java/com/gausslab/timeoffrequester/ui/login/LoginScreen.kt
+++ app/src/main/java/com/gausslab/timeoffrequester/ui/login/LoginScreen.kt
@@ -1,12 +1,26 @@
 package com.gausslab.timeoffrequester.ui.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -26,17 +40,56 @@ fun NavController.navigateToLoginScreen(navOptions: NavOptions? = null) {
     this.navigate(route = loginScreenRoute, navOptions = navOptions)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(
     viewModel: LoginViewModel = hiltViewModel()
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Button(onClick = { viewModel.onEvent(LoginUiEvent.GoToMainScreenPressed) }) {
-            Text("Go to main screen")
+    val formState by viewModel.formState
+    val userEmail = formState.userEmail
+    val password = formState.password
+    val isFormValid by viewModel.isFormValid
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(100.dp))
+            OutlinedTextField(
+                modifier = Modifier,
+                value = userEmail,
+                onValueChange = {viewModel.onEvent(LoginUiEvent.UserEmailChanged(it))},
+                label = { Text(text = "Email")},
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                modifier = Modifier,
+                value = password,
+                onValueChange = {viewModel.onEvent(LoginUiEvent.PasswordChanged(it))},
+                label = { Text(text = "Password")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Row() {
+//                Checkbox(
+//                    checked = ,
+//                    onCheckedChange =
+//                )
+                Text(text = "자동 로그인")
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(text = "password찾기")
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                onClick = { viewModel.onEvent(LoginUiEvent.LoginButtonPressed) },
+            enabled = isFormValid) {
+                Text("Login")
+            }
         }
     }
+
 }
--- app/src/main/java/com/gausslab/timeoffrequester/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/gausslab/timeoffrequester/ui/login/LoginViewModel.kt
@@ -1,23 +1,65 @@
 package com.gausslab.timeoffrequester.ui.login
 
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import com.gausslab.timeoffrequester.ui.main.navigateToMainScreen
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val navController: NavHostController
 ) : ViewModel() {
+    private val _formState: MutableState<LoginFormState> = mutableStateOf(LoginFormState())
+    val formState: State<LoginFormState> = _formState
+    private val _isFormValid: MutableState<Boolean> = mutableStateOf(false)
+    val isFormValid:State<Boolean> = _isFormValid
+
+    private fun checkIfFormIsValid(){
+        val form by _formState
+        var isValid = true
+        if(form.userEmail.isEmpty() || !form.userEmail.contains("@")){
+            isValid = false
+        } else if (form.password.isEmpty() || form.password.length<4){
+            isValid = false
+        }
+        _isFormValid.value = isValid
+
+    }
+
     fun onEvent(event: LoginUiEvent) {
         when(event){
-            LoginUiEvent.GoToMainScreenPressed -> navController.navigateToMainScreen()
+            is LoginUiEvent.UserEmailChanged -> {
+                _formState.value = _formState.value.copy(
+                    userEmail = event.userEmail
+                )
+                checkIfFormIsValid()
+            }
+            is LoginUiEvent.PasswordChanged -> {
+                _formState.value = _formState.value.copy(
+                    password = event.password
+                )
+                checkIfFormIsValid()
+            }
+            LoginUiEvent.LoginButtonPressed -> navController.navigateToMainScreen()
         }
     }
 }
 
+data class LoginFormState(
+    val userEmail: String ="",
+    val password: String=""
+)
+
 sealed class LoginUiEvent {
-    object GoToMainScreenPressed : LoginUiEvent()
+    data class UserEmailChanged(val userEmail: String): LoginUiEvent()
+    data class PasswordChanged(val password: String):LoginUiEvent()
+    object LoginButtonPressed : LoginUiEvent()
 }
